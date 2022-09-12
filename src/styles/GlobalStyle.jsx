db64d81cbc37267fa577633b5a2ddbbff7f272d7
ryanvs <ryanoliveirasp@gmail.com>
2022-09-12 14:19:35 -0700

    scroll modify

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -5,14 +5,33 @@ const GlobalStyle = createGlobalStyle` //Styled global App
     margin: 0;
     box-sizing: border-box; //faz com que o navegador não calcule a dimensão de um elemento somando bordas e margens com altura e largura.
 }
+/* ===== Scrollbar CSS ===== */
+  /* Firefox */
+  * {
+    scrollbar-width: auto;
+    scrollbar-color: #FAD711 #ffffff;
+  }
 
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 16px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #d3d3d3;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #282828;
+    border: 1px solid #282828;
+  }
 body {
-    background-color:#FAD711;
+    background-color: #FAD711;
     font: 400 14px Roboto, sans-serif;
 }
 :root {
     --bgCollor: #FAD711;
-    --primary: #FAD711;
+    --primary: #282828;
     --text: #282828;
     --subTitle: #929292;
 }
